b569fdb87f492e68254b1a71f0b3e6d6ec4b6e91
Borges dev <leosull50@hotmail.com>
2025-01-15 19:12:39 -0300

    FEAT: TRANSACTIONS CRUD

---
+++ src/modules/transactions/services/transactions.service.ts
@@ -0,0 +1,108 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { CreateTransactionDto } from '../dto/create-transaction.dto';
+import { UpdateTransactionDto } from '../dto/update-transaction.dto';
+import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repository';
+import { ValidateBankAccountOwnershipService } from '../../bank-accounts/services/validate-bank-account-ownership.service';
+import { ValidateCategoryOwnershipService } from '../../categories/services/validate-category-ownership.service';
+import { ValidateTransactionOwnershipService } from './validate-transaction-ownership.service';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    private readonly transactionsRepo: TransactionsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+    private readonly validateCategoryOwnerShipService: ValidateCategoryOwnershipService,
+    private readonly validateTransactionOwnershipService: ValidateTransactionOwnershipService
+  ) {}
+
+  async create(userId: string, createTransactionDto: CreateTransactionDto) {
+    const { bankAccountId, categoryId, date, name, type, value } =
+      createTransactionDto;
+
+    await this.validateEntitiesOwnerShip({ userId, bankAccountId, categoryId });
+
+    return this.transactionsRepo.create({
+      data: {
+        userId,
+        bankAccountId,
+        categoryId,
+        date,
+        name,
+        type,
+        value
+      }
+    });
+  }
+
+  findAllByUserId(userId: string) {
+    return this.transactionsRepo.findMany({
+      where: {
+        userId
+      }
+    });
+  }
+
+  async update(
+    userId: string,
+    transactionId: string,
+    updateTransactionDto: UpdateTransactionDto
+  ) {
+    const { bankAccountId, categoryId, date, name, type, value } =
+      updateTransactionDto;
+    await this.validateEntitiesOwnerShip({
+      userId,
+      bankAccountId,
+      categoryId,
+      transactionId
+    });
+    return this.transactionsRepo.update({
+      where: {
+        id: transactionId
+      },
+      data: {
+        bankAccountId,
+        categoryId,
+        date,
+        name,
+        type,
+        value
+      }
+    });
+  }
+
+  async remove(userId: string, transactionId: string) {
+    await this.validateEntitiesOwnerShip({ userId, transactionId });
+    await this.transactionsRepo.delete({
+      where: { id: transactionId }
+    });
+    return null
+  }
+
+  private async validateEntitiesOwnerShip({
+    userId,
+    bankAccountId,
+    categoryId,
+    transactionId
+  }: {
+    userId: string;
+    bankAccountId?: string;
+    categoryId?: string;
+    transactionId?: string;
+  }) {
+    await Promise.all([
+      transactionId &&
+        this.validateTransactionOwnershipService.validate(
+          userId,
+          transactionId
+        ),
+      bankAccountId &&
+        this.validateBankAccountOwnershipService.validate(
+          userId,
+          bankAccountId
+        ),
+      categoryId &&
+        this.validateCategoryOwnerShipService.validate(userId, categoryId)
+    ]);
+  }
+}
--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -4,6 +4,7 @@ import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/users.repositories';
 import { CategoriesRepository } from './repositories/categories.repositories';
 import { BankAccountsRepository } from './repositories/bank-accounts.repositories';
+import { TransactionsRepository } from './repositories/transactions.repository';
 
 @Global()
 @Module({
@@ -11,8 +12,14 @@ import { BankAccountsRepository } from './repositories/bank-accounts.repositorie
     PrismaService,
     UsersRepository,
     CategoriesRepository,
-    BankAccountsRepository
+    BankAccountsRepository,
+    TransactionsRepository
   ],
-  exports: [UsersRepository, CategoriesRepository, BankAccountsRepository]
+  exports: [
+    UsersRepository,
+    CategoriesRepository,
+    BankAccountsRepository,
+    TransactionsRepository
+  ]
 })
 export class DatabaseModule {}
